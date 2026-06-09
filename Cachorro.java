4cb76a5db4938194df9d10cc4cbb6f45d1a0dbdf
loryhhh <farmsandra63@gmail.com>
2026-06-09 14:55:27 -0300

    Cachorro.java

--- Cachorro.java
+++ Cachorro.java
@@ -1,11 +1,24 @@
-	class Cachorro extends Animal {
-	    Cachorro(String nome, int idade, String raca, boolean faminto, String dono) {
-	        super(nome, idade, raca, faminto, dono);
-	    }
+package petshop;
 
-	    @Override
-	    public void emitirSom() {
-	        System.out.println("Au au!");
-	    }
+class Cachorro extends Animal implements AtendivelNoEstetica {
+    Cachorro(String nome, int idade, String raca, boolean faminto, String dono) {
+        super(nome, idade, raca, faminto, dono);
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Au au!");
+    }
+
+	@Override
+	public void darBanho() {
+		// TODO Auto-generated method stub
+		
 	}
 
+	@Override
+	public void cortarUnhas() {
+		// TODO Auto-generated method stub
+		
+	}
+}	
