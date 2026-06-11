73aea0332a1971919bcb8e4d4b3ad6e1aab320f5
loryhhh <farmsandra63@gmail.com>
2026-06-11 14:19:38 -0300

    PetShop.java

--- PetShop.java
+++ PetShop.java
@@ -1,27 +1,28 @@
-public class PetShop {
-    public static void main(String[] args) {
+package petshop;
 
-        Animal[] filaAtendimento = new Animal[2];
+public class Petshop {
+	public static void main(String[] args) {
+		Animal[] filaAtendimento = new Animal[2];
+		filaAtendimento[0] = new Cachorro("Rex", 5, "Labrador", true, "João");
+		filaAtendimento[1] = new Gato("Mia", 3, false, "Maria", true);
+		AtendivelNoEstetica[] filaEstetica = new AtendivelNoEstetica[1];
+		filaEstetica[0] = (AtendivelNoEstetica) filaAtendimento[0];
+		for (int i = 0; i < filaAtendimento.length; i++) {
+			System.out.println("Animal " + (i + 1) + ":");
+			filaAtendimento[i].emitirSom();
+			if (filaAtendimento[i] instanceof AtendivelNoEstetica) {
 
-        filaAtendimento[0] =
-                new Cachorro("Rex", 5, "Labrador", true, "João");
+				AtendivelNoEstetica atendivel = (AtendivelNoEstetica) filaAtendimento[i];
 
-        filaAtendimento[1] =
-                new Gato("Mia", 3, false, "Maria", true);
-
-        for (int i = 0; i < filaAtendimento.length; i++) {
-
-            System.out.println("Animal " + (i + 1) + ":");
-
-            filaAtendimento[i].emitirSom();
-
-            System.out.println("Nome: " + filaAtendimento[i].getidade());
-            System.out.println("Raça: " + filaAtendimento[i].getraca());
-            System.out.println("Idade: " + filaAtendimento[i].getnome());
-            System.out.println("Faminto: " + filaAtendimento[i].isfaminto());
-            System.out.println("O dono do pet é: " + filaAtendimento[i].getdono());
-            Animal.totalDeAnimais = Animal.totalDeAnimais + 1;
-			System.out.println("Total de animais no pet shop: "+ + Animal.totalDeAnimais);
-        }
-    }
+				atendivel.darBanho();
+				atendivel.cortarUnhas();
+			}
+			System.out.println("Nome: " + filaAtendimento[i].getnome());
+			System.out.println("Raça: " + filaAtendimento[i].getraca());
+			System.out.println("Idade: " + filaAtendimento[i].getidade());
+			System.out.println("Faminto: " + filaAtendimento[i].isfaminto());
+			System.out.println("O dono do pet é: " + filaAtendimento[i].getdono());
+			System.out.println("Total de animais no pet shop: " + Animal.totalDeAnimais);
+		}
+	}
 }
